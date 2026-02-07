3d270ed90c2bd860faa973522704439bd60e9a97
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-02-07 12:17:43 +0100

    config(docker): enable rootless mode

--- modules/programs/dev/virtualization/docker/docker.nix
+++ modules/programs/dev/virtualization/docker/docker.nix
@@ -1,5 +1,11 @@
 {
   flake.modules.nixos.docker = {
-    virtualisation.docker.enable = true;
+    virtualisation.docker = {
+      enable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+    };
   };
 }
