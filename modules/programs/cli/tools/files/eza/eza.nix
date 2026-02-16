d9122ff1746e18a5ab8637611c05e699377c942f
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-02-16 13:26:10 +0100

    config(eza): enable git & icons

--- modules/programs/cli/tools/files/eza/eza.nix
+++ modules/programs/cli/tools/files/eza/eza.nix
@@ -1,6 +1,11 @@
 {
   flake.modules.homeManager.eza = {
-    programs.eza.enable = true;
+    programs.eza = {
+      enable = true;
+      enableFishIntegration = true;
+      git = true;
+      icons = "auto";
+    };
 
     programs.fish.shellAliases = {
       lt = "eza -T";
