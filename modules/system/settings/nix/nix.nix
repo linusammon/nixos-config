cbdeb701cca32f3dcdf75e278c158adc02c09eb6
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-02-18 17:40:15 +0100

    config(nix): tweak nix settings

--- modules/system/settings/nix/nix.nix
+++ modules/system/settings/nix/nix.nix
@@ -5,16 +5,6 @@
 
       download-buffer-size = 1024 * 1024 * 1024;
 
-      substituters = [
-        "https://cache.nixos.org?priority=10"
-        "https://nix-community.cachix.org"
-      ];
-
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-
       experimental-features = [
         "nix-command"
         "flakes"
@@ -25,12 +15,5 @@
         "@wheel"
       ];
     };
-
-    nix.extraOptions = ''
-      warn-dirty = false
-      keep-outputs = true
-    '';
-
-    documentation.nixos.enable = false;
   };
 }
