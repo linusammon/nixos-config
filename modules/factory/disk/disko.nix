a04589ddd29de943fc73121cb55c411a4b00d06d
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-02-25 11:42:16 +0100

    fix disko curly brackets

--- modules/factory/disk/disko.nix
+++ modules/factory/disk/disko.nix
@@ -44,7 +44,7 @@
       disko.devices = {
         disk = {
           main = {
-            device = device;
+            inherit device;
             type = "disk";
             content = {
               type = "gpt";
@@ -69,49 +69,49 @@
               };
             };
           };
+        };
 
-          zpool.zroot = {
-            type = "zpool";
-            rootFsOptions = {
-              # https://wiki.archlinux.org/title/Install_Arch_Linux_on_ZFS
-              acltype = "posixacl";
-              atime = "off";
-              compression = "zstd";
-              mountpoint = "none";
-              xattr = "sa";
-            };
-            options.ashift = "12";
-
-            datasets = {
-              "local" = {
-                type = "zfs_fs";
-                options.mountpoint = "none";
-              };
-              "local/home" = {
-                type = "zfs_fs";
-                mountpoint = "/home";
-                # Used by services.zfs.autoSnapshot options.
-                options."com.sun:auto-snapshot" = "true";
-              };
-              "local/nix" = {
-                type = "zfs_fs";
-                mountpoint = "/nix";
-                options."com.sun:auto-snapshot" = "false";
-              };
-              "local/persist" = {
-                type = "zfs_fs";
-                mountpoint = "/persist";
-                options."com.sun:auto-snapshot" = "false";
-              };
-              "local/root" = {
-                type = "zfs_fs";
-                mountpoint = "/";
-                options."com.sun:auto-snapshot" = "false";
-                postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot/local/root@blank$' || zfs snapshot zroot/local/root@blank && zfs hold nixos-impermanence zroot/local/root@blank";
-              };
-            }
-            // swapDataset;
+        zpool.zroot = {
+          type = "zpool";
+          rootFsOptions = {
+            # https://wiki.archlinux.org/title/Install_Arch_Linux_on_ZFS
+            acltype = "posixacl";
+            atime = "off";
+            compression = "zstd";
+            mountpoint = "none";
+            xattr = "sa";
           };
+          options.ashift = "12";
+
+          datasets = {
+            "local" = {
+              type = "zfs_fs";
+              options.mountpoint = "none";
+            };
+            "local/home" = {
+              type = "zfs_fs";
+              mountpoint = "/home";
+              # Used by services.zfs.autoSnapshot options.
+              options."com.sun:auto-snapshot" = "true";
+            };
+            "local/nix" = {
+              type = "zfs_fs";
+              mountpoint = "/nix";
+              options."com.sun:auto-snapshot" = "false";
+            };
+            "local/persist" = {
+              type = "zfs_fs";
+              mountpoint = "/persist";
+              options."com.sun:auto-snapshot" = "false";
+            };
+            "local/root" = {
+              type = "zfs_fs";
+              mountpoint = "/";
+              options."com.sun:auto-snapshot" = "false";
+              postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot/local/root@blank$' || zfs snapshot zroot/local/root@blank && zfs hold nixos-impermanence zroot/local/root@blank";
+            };
+          }
+          // swapDataset;
         };
       };
     };
