e7bd262b54c9f9ca6a16990b23fca98904abf931
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-03-09 13:07:18 +0100

    chore(programs): remove yazi module

--- modules/features/helix/settings.nix
+++ modules/features/helix/settings.nix
@@ -34,15 +34,6 @@
             pagedown = "no_op";
             home = "no_op";
             end = "no_op";
-            space = {
-              space = [
-                ":sh rm -f /tmp/unique-file"
-                ":insert-output yazi %{buffer_name} --chooser-file=/tmp/unique-file"
-                ":insert-output echo '\x1b[?1049h\x1b[?2004h' > /dev/tty"
-                ":open %sh{cat /tmp/unique-file}"
-                ":redraw"
-              ];
-            };
           };
         };
       };
