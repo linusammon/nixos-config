7d0c4a20925c62c375e4b387edecf89233a9e8f8
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-03-26 11:15:38 +0100

    chore(starship): clean up

--- modules/features/starship/starship.nix
+++ modules/features/starship/starship.nix
@@ -2,9 +2,6 @@
   flake.modules.homeManager.starship = {
     programs.starship = {
       enable = true;
-      enableFishIntegration = true;
-      enableInteractive = true;
-
       settings = {
         line_break.disabled = true;
       };
