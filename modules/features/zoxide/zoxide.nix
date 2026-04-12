ff4aae313f80780dcdf130d79e68700127d8afb5
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-04-12 12:26:47 +0200

    feat(zoxide): migrate to nixos module

--- modules/features/zoxide/zoxide.nix
+++ modules/features/zoxide/zoxide.nix
@@ -1,8 +1,21 @@
 {
-  flake.modules.homeManager.zoxide = {
-    programs.zoxide = {
-      enable = true;
-      options = [ "--cmd cd" ];
+  flake.modules.nixos.zoxide =
+    { pkgs, lib, ... }:
+    {
+      environment = {
+        systemPackages = [ pkgs.zoxide ];
+      };
+
+      programs =
+        let
+          exe = lib.getExe pkgs.zoxide;
+        in
+        {
+          fish = {
+            interactiveShellInit = ''
+              ${exe} init fish --cmd cd | source
+            '';
+          };
+        };
     };
-  };
 }
