24b4b01d4f8afecf32a1339e5d8cded3f110f9f0
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-04-12 16:30:11 +0200

    chore(zoxide): clean up formatting

--- modules/features/zoxide/zoxide.nix
+++ modules/features/zoxide/zoxide.nix
@@ -2,20 +2,16 @@
   flake.modules.nixos.zoxide =
     { pkgs, lib, ... }:
     {
-      environment = {
-        systemPackages = [ pkgs.zoxide ];
-      };
+      environment.systemPackages = [ pkgs.zoxide ];
 
       programs =
         let
           exe = lib.getExe pkgs.zoxide;
         in
         {
-          fish = {
-            interactiveShellInit = ''
-              ${exe} init fish --cmd cd | source
-            '';
-          };
+          fish.interactiveShellInit = ''
+            ${exe} init fish --cmd cd | source
+          '';
         };
     };
 }
