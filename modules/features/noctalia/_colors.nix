001a1a07da32d82fb0f610c0a417e02051e0e5b6
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-04-16 19:31:20 +0200

    feat(noctalia): migrate to nixos module

---
+++ modules/features/noctalia/_colors.nix
@@ -0,0 +1,19 @@
+{ self }:
+{
+  mPrimary = self.theme.base0D;
+  mOnPrimary = self.theme.base00;
+  mSecondary = self.theme.base0E;
+  mOnSecondary = self.theme.base00;
+  mTertiary = self.theme.base0C;
+  mOnTertiary = self.theme.base00;
+  mError = self.theme.base08;
+  mOnError = self.theme.base00;
+  mSurface = self.theme.base00;
+  mOnSurface = self.theme.base05;
+  mHover = self.theme.base0C;
+  mOnHover = self.theme.base00;
+  mSurfaceVariant = self.theme.base01;
+  mOnSurfaceVariant = self.theme.base04;
+  mOutline = self.theme.base03;
+  mShadow = self.theme.base00;
+}
