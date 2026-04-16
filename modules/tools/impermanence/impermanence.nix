f6ac2cc86dde56b5e118ff224d5b3867dc6953b8
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-04-16 18:20:14 +0200

    fix(impermanence): add missing config attribute

--- modules/tools/impermanence/impermanence.nix
+++ modules/tools/impermanence/impermanence.nix
@@ -39,41 +39,43 @@
         };
       };
 
-      environment.persistence = {
-        "/persistent/user".users."${cfg.user.name}" = {
-          directories = cfg.user.directories;
-          files = cfg.user.files;
+      config = {
+        environment.persistence = {
+          "/persistent/user".users."${cfg.user.name}" = {
+            directories = cfg.user.directories;
+            files = cfg.user.files;
+          };
+
+          "/persistent/system" = {
+            directories = cfg.directories;
+            files = cfg.files;
+          };
         };
 
-        "/persistent/system" = {
-          directories = cfg.directories;
-          files = cfg.files;
-        };
-      };
+        boot.initrd.postResumeCommands = lib.mkAfter ''
+          mkdir /btrfs_tmp
+            mount /dev/nvme0n1p2 /btrfs_tmp
+            if [[ -e /btrfs_tmp/root ]]; then
+              mkdir -p /btrfs_tmp/old_roots
+              timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+              mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+            fi
 
-      boot.initrd.postResumeCommands = lib.mkAfter ''
-        mkdir /btrfs_tmp
-          mount /dev/nvme0n1p2 /btrfs_tmp
-          if [[ -e /btrfs_tmp/root ]]; then
-            mkdir -p /btrfs_tmp/old_roots
-            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-            mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-          fi
+            delete_subvolume_recursively() {
+              IFS=$'\n'
+              for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                delete_subvolume_recursively "/btrfs_tmp/$i"
+              done
+              btrfs subvolume delete "$1"
+            }
 
-          delete_subvolume_recursively() {
-            IFS=$'\n'
-            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-              delete_subvolume_recursively "/btrfs_tmp/$i"
+            for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+              delete_subvolume_recursively "$i"
             done
-            btrfs subvolume delete "$1"
-          }
 
-          for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-            delete_subvolume_recursively "$i"
-          done
-
-          btrfs subvolume create /btrfs_tmp/root
-          umount /btrfs_tmp
-      '';
+            btrfs subvolume create /btrfs_tmp/root
+            umount /btrfs_tmp
+        '';
+      };
     };
 }
