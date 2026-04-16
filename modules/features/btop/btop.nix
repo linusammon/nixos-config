8d586b57c9d5eaeaef409b1fb2c2dfae2aa42989
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-04-16 19:32:41 +0200

    chore(btop): move wrapper into feature file

--- modules/features/btop/btop.nix
+++ modules/features/btop/btop.nix
@@ -1,5 +1,25 @@
-{ self, ... }:
+{ self, inputs, ... }:
 {
+  perSystem =
+    { pkgs, ... }:
+    let
+      conf = pkgs.writeText "btop.conf" ''
+        theme_background = false
+        rounded_corners = false
+        cpu_single_graph = true
+        clock_format = ""
+      '';
+    in
+    {
+      packages.btop = inputs.wrappers.lib.wrapPackage {
+        inherit pkgs;
+        package = pkgs.btop;
+        flags = {
+          "--config" = conf;
+        };
+      };
+    };
+
   flake.modules.nixos.btop =
     { pkgs, ... }:
     {
