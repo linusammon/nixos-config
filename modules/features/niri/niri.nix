d57737072908fe304887feebcffe15a6560e93ab
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-04-16 20:42:05 +0200

    feat(niri): migrate to nixos module

--- modules/features/niri/niri.nix
+++ modules/features/niri/niri.nix
@@ -1,21 +1,26 @@
-{ inputs, ... }:
+{ self, inputs, ... }:
 {
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.niri = inputs.wrappers.wrappers.niri.wrap {
+        inherit pkgs;
+        package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.default;
+        settings = import ./_settings.nix;
+      };
+    };
+
   flake.modules.nixos.niri =
     { pkgs, ... }:
     {
-      nixpkgs.overlays = [ inputs.niri.overlays.default ];
-
-      programs.niri.enable = true;
+      programs.niri = {
+        enable = true;
+        package = self.packages.${pkgs.stdenv.hostPlatform.system}.niri;
+      };
 
       environment.systemPackages = with pkgs; [
-        bibata-cursors
         xwayland-satellite
-      ];
-
-      home-manager.sharedModules = [
-        {
-          home.file.".config/niri/config.kdl".source = ./config.kdl;
-        }
+        bibata-cursors
       ];
     };
 }
