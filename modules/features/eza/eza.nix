82222004bd0134846c0d1c8dbfadad431466b2d2
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-04-16 19:33:05 +0200

    chore(eza): move wrapper into feature file

--- modules/features/eza/eza.nix
+++ modules/features/eza/eza.nix
@@ -1,5 +1,22 @@
-{ self, ... }:
+{ self, inputs, ... }:
 {
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.eza = inputs.wrappers.lib.wrapPackage {
+        inherit pkgs;
+        package = pkgs.eza;
+        flags = {
+          "--icons" = true;
+          "--group-directories-first" = true;
+          "--hyperlink" = true;
+          "--total-size" = true;
+          "--no-user" = true;
+          "--time-style" = "+%Y-%m-%d %H:%M";
+        };
+      };
+    };
+
   flake.modules.nixos.eza =
     { pkgs, ... }:
     {
