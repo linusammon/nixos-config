9ad37e2fda666b4e80f23c8c0dcd224e97025500
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-04-19 12:27:54 +0200

    chore(flake): remove firefox-addons input

--- flake.nix
+++ flake.nix
@@ -69,11 +69,6 @@
         flake-parts.follows = "flake-parts";
       };
     };
-
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
