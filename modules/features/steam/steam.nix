b58e783486c4494f1ab25702399b228ffa6539c9
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-04-20 10:21:51 +0200

    feat: replace gaming module with steam specialisation

---
+++ modules/features/steam/steam.nix
@@ -0,0 +1,16 @@
+{
+  flake.modules.nixos.steam =
+    { ... }:
+    {
+      specialisation.steam.configuration = {
+        programs = {
+          steam.enable = true;
+          gamemode.enable = true;
+        };
+      };
+
+      persistence.user.files = [
+        ".local/share/steam"
+      ];
+    };
+}
