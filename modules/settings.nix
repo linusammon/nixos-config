a2fd44d40564bbe43dbfa1954b39428497849b80
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-05-16 22:52:15 +0200

    feat(hosts): add ISO with interactive installer

--- modules/settings.nix
+++ modules/settings.nix
@@ -60,6 +60,14 @@
             "root"
             "@wheel"
           ];
+
+          extra-substituters = [
+            "https://linusammon.cachix.org"
+          ];
+
+          extra-trusted-public-keys = [
+            "linusammon.cachix.org-1:g0X9oaHIZ5j48dsqfu8ZEdAp6HA2pH7Buv5Ye698qfc="
+          ];
         };
       };
 
