7fdfa4ccc2ec696a33fc50c0cc9b5e343824c104
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-05-24 15:10:43 +0200

    refactor(users): merge xdg user dirs config

--- modules/users.nix
+++ modules/users.nix
@@ -1,25 +1,46 @@
 {
   flake.modules.nixos.common =
-    { config, ... }:
+    { pkgs, config, ... }:
     let
       inherit (config.custom.constants) user;
     in
     {
-      users = {
-        users = {
-          root = {
-            initialPassword = "password";
-          };
+      users.users = {
+        root = {
+          initialPassword = "password";
+        };
 
-          ${user} = {
-            initialPassword = "password";
-            isNormalUser = true;
-            extraGroups = [
-              "networkmanager"
-              "wheel"
-            ];
-          };
+        ${user} = {
+          initialPassword = "password";
+          isNormalUser = true;
+          extraGroups = [
+            "networkmanager"
+            "wheel"
+          ];
         };
       };
+
+      environment = {
+        systemPackages = [
+          pkgs.xdg-user-dirs
+        ];
+
+        etc."xdg/user-dirs.defaults".text = ''
+          DOWNLOAD=Downloads
+          DOCUMENTS=Documents
+          PROJECTS=Projects
+          PICTURES=Pictures
+          VIDEOS=Videos
+          MUSIC=Music
+        '';
+      };
+
+      custom.persist.user.directories = [
+        "Documents"
+        "Projects"
+        "Pictures"
+        "Videos"
+        "Music"
+      ];
     };
 }
