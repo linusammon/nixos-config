10c53efa213bc5966b6786c740db9b5a7f2feb69
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-05-24 20:42:02 +0200

    feat: add i18n locale configuration for de_CH/en_GB

--- modules/settings.nix
+++ modules/settings.nix
@@ -1,4 +1,3 @@
-{ ... }:
 {
   flake.modules.nixos.common =
     { config, ... }:
@@ -11,6 +10,22 @@
       console.keyMap = "de";
 
       time.timeZone = "Europe/Zurich";
+
+      i18n = {
+        defaultLocale = "en_GB.UTF-8";
+        extraLocales = [ "de_CH.UTF-8/UTF-8" ];
+        extraLocaleSettings = {
+          LC_ADDRESS = "de_CH.UTF-8";
+          LC_IDENTIFICATION = "de_CH.UTF-8";
+          LC_MEASUREMENT = "de_CH.UTF-8";
+          LC_MONETARY = "de_CH.UTF-8";
+          LC_NAME = "de_CH.UTF-8";
+          LC_NUMERIC = "de_CH.UTF-8";
+          LC_PAPER = "de_CH.UTF-8";
+          LC_TELEPHONE = "de_CH.UTF-8";
+          LC_TIME = "en_GB.UTF-8";
+        };
+      };
 
       system.stateVersion = "26.05";
     };
