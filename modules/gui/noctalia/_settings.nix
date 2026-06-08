c9fe869b87087fc701d7b0a4c087c71473ec9cb3
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-06-08 16:39:08 +0200

    refactor(gui/noctalia): reorganize settings by context

--- modules/gui/noctalia/_settings.nix
+++ modules/gui/noctalia/_settings.nix
@@ -1,13 +1,66 @@
 { self }:
 {
-  backdrop = {
-    enabled = true;
+  shell = {
+    corner_radius_scale = 0.0;
+    font_family = self.fonts.sans;
+    screen_time_enabled = true;
+    launch_apps_as_systemd_services = true;
+    niri_overview_type_to_launch_enabled = true;
+
+    panel = {
+      launcher_categories = false;
+      open_near_click_control_center = true;
+      session_placement = "centered";
+      transparency_mode = "soft";
+    };
+
+    screenshot.directory = "/home/linus/Pictures/screenshots";
+
+    settings_show_advanced = true;
+    setup_wizard_enabled = false;
+    telemetry_enabled = false;
+  };
+
+  control_center = {
+    sidebar = "none";
+    shortcuts = [
+      "screen_time"
+      "wallpaper"
+      "sysmon"
+      "power_profile"
+    ];
+  };
+
+  location.auto_locate = true;
+
+  backdrop.enabled = true;
+
+  nightlight.enabled = true;
+
+  theme = {
+    mode = "dark";
+    source = "custom";
+    custom_palette = "palette";
+  };
+
+  wallpaper = {
+    directory = "/home/linus/Pictures";
+    default.path = "/home/linus/Pictures/wp15769140-linkin-park-from-zero-wallpapers.png";
   };
 
   bar.default = {
     auto_hide = true;
-    capsule_radius = 0;
     background_opacity = 0.9;
+    capsule_radius = 0;
+    font_weight = "bold";
+    margin_edge = 0;
+    margin_ends = 0;
+    padding = 10;
+    radius = 0;
+    widget_spacing = 10;
+    start = [
+      "taskbar"
+    ];
     center = [
       "clock"
     ];
@@ -22,74 +75,16 @@
       "network"
       "battery"
     ];
-    font_weight = "bold";
-    margin_edge = 0;
-    margin_ends = 0;
-    padding = 10;
-    radius = 0;
-    start = [ "taskbar" ];
-    widget_spacing = 10;
   };
-
-  nightlight = {
-    enabled = true;
-  };
-
-  shell = {
-    setup_wizard_enabled = false;
-    corner_radius_scale = 0.0;
-    font_family = self.fonts.sans;
-    niri_overview_type_to_launch_enabled = true;
-    settings_show_advanced = true;
-    screen_time_enabled = true;
-    launch_apps_as_systemd_services = true;
-    telemetry_enabled = false;
-
-    screenshot.directory = "/home/linus/Pictures/screenshots";
-
-    panel = {
-      open_near_click_control_center = true;
-      session_placement = "centered";
-      launcher_categories = false;
-      transparency_mode = "soft";
-    };
-
-    control_center = {
-      sidebar = "none";
-      shortcuts = [
-        "screen_time"
-        "wallpaper"
-        "sysmon"
-        "power_profile"
-      ];
-    };
-  };
-
-  theme = {
-    mode = "dark";
-    source = "custom";
-    custom_palette = "palette";
-    templates = {
-      enable_builtin_templates = false;
-      enable_community_templates = false;
-    };
-  };
-
-  wallpaper = {
-    directory = "/home/linus/Pictures";
-    default.path = "/home/linus/Pictures/wp15769140-linkin-park-from-zero-wallpapers.png";
-  };
-
-  location.auto_locate = true;
 
   widget = {
-    clock = {
-      format = "{:%A  %H:%M}";
-    };
+    clock.format = "{:%A  %H:%M}";
 
-    network = {
-      show_label = false;
-    };
+    tray.drawer = true;
+
+    network.show_label = false;
+
+    volume.show_label = false;
 
     screen_recorder = {
       script = "scripts/screen_recorder.lua";
@@ -98,19 +93,8 @@
 
     taskbar = {
       group_by_workspace = true;
-    };
-
-    tray = {
-      drawer = true;
-    };
-
-    volume = {
-      show_label = false;
-    };
-
-    taskbar = {
       hide_empty_workspaces = true;
-      inactive_opacity = 0.80000000000000004;
+      inactive_opacity = 0.8;
       show_active_indicator = false;
       workspace_group_capsule = false;
     };
