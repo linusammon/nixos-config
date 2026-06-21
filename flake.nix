1f7dc0a0e39d4849dede41eab869dc928970f0f6
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-06-21 09:25:16 +0200

    feat: migrate flake inputs to tack

--- flake.nix
+++ flake.nix
@@ -1,77 +1,32 @@
 {
-  inputs = {
-    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
-
-    flake-parts.url = "github:hercules-ci/flake-parts";
-
-    nix-cachyos-kernel = {
-      url = "github:xddxdd/nix-cachyos-kernel/release";
-      inputs.flake-parts.follows = "flake-parts";
-    };
-
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    impermanence = {
-      url = "github:nix-community/impermanence";
-      inputs = {
-        nixpkgs.follows = "";
-        home-manager.follows = "";
-      };
-    };
-
-    wrappers = {
-      url = "github:BirdeeHub/nix-wrapper-modules";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hjem = {
-      url = "github:feel-co/hjem";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    monique = {
-      url = "github:ToRvaLDz/monique";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    noctalia = {
-      url = "github:noctalia-dev/noctalia";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nixcord = {
-      url = "github:FlameFlag/nixcord";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-parts.follows = "flake-parts";
-      };
-    };
-
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
   outputs =
-    inputs:
+    { self, ... }:
     let
-      inherit (inputs.nixpkgs) lib;
+      tackInputs = import ./.tack;
+
+      inherit (tackInputs.nixpkgs) lib;
       inherit (lib.fileset) toList fileFilter;
 
       isNixModule = file: file.hasExt "nix" && file.name != "flake.nix" && !lib.hasPrefix "_" file.name;
 
-      importTree = path: toList (fileFilter isNixModule path);
+      importTree =
+        path:
+        toList (
+          lib.fileset.intersection (lib.fileset.difference path (path + "/.tack")) (
+            fileFilter isNixModule path
+          )
+        );
 
-      mkFlake = inputs.flake-parts.lib.mkFlake { inherit inputs; };
+      inputs = tackInputs // {
+        self = self';
+      };
+
+      self' = outputs // {
+        inherit inputs;
+        inherit (self) outPath;
+      };
+
+      outputs = tackInputs.flake-parts.lib.mkFlake { inherit inputs; } { imports = importTree ./.; };
     in
-    mkFlake { imports = importTree ./.; };
+    outputs;
 }
