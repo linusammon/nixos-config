0443e4473a2c0780718b690a4d39389e45cba05d
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-06-25 23:35:32 +0200

    feat(cli): add gh module

---
+++ modules/cli/gh.nix
@@ -0,0 +1,25 @@
+{ inputs, ... }: {
+  modules.nixos.cli.gh =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    let
+      gh = inputs.nix-wrapper-modules.lib.wrapPackage {
+        inherit pkgs;
+        package = pkgs.gh;
+        env.GH_CONFIG_DIR = pkgs.writeTextDir "/config.yml" (lib.strings.toJSON { version = 1; });
+        runShell = [
+          ''
+            GITHUB_TOKEN=$(cat "${config.sops.secrets.github-token.path}")
+            export GITHUB_TOKEN
+          ''
+        ];
+      };
+    in
+    {
+      environment.systemPackages = [ gh ];
+    };
+}
