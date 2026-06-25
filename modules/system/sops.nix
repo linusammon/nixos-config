6ee153f4a6269f2924134532a30a02762cbfbe8f
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-06-25 23:35:32 +0200

    feat(system/sops): add github-token secret

--- modules/system/sops.nix
+++ modules/system/sops.nix
@@ -1,12 +1,15 @@
 { inputs, ... }: {
-  modules.nixos.system.sops = _: {
+  modules.nixos.system.sops = { user, ... }: {
     imports = [ inputs.sops-nix.nixosModules.sops ];
 
     sops = {
       defaultSopsFile = ../../secrets/secrets.yaml;
       age.keyFile = "/etc/age/key.txt";
 
-      secrets.user-password.neededForUsers = true;
+      secrets = {
+        user-password.neededForUsers = true;
+        github-token.owner = user;
+      };
     };
   };
 }
