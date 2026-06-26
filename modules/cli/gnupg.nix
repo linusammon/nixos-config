193a63ce97fe536f8705e3c2f4e96f13351bcc25
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-06-26 21:39:42 +0200

    feat(cli/gnupg): manage GPG key with sops

--- modules/cli/gnupg.nix
+++ modules/cli/gnupg.nix
@@ -1,12 +1,34 @@
 {
-  modules.nixos.cli.gnupg = _: {
-    programs.gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true;
-    };
+  modules.nixos.cli.gnupg =
+    {
+      config,
+      pkgs,
+      args,
+      ...
+    }:
+    {
+      programs.gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
+      };
 
-    custom.persist.user.directories = [
-      ".gnupg"
-    ];
-  };
+      systemd.services.import-gpg-key = {
+        wantedBy = [ "multi-user.target" ];
+        after = [ "local-fs.target" ];
+        serviceConfig = {
+          Type = "oneshot";
+          User = args.user;
+          RemainAfterExit = true;
+        };
+        script = ''
+          if [ -f '${config.sops.secrets.gpg-private-key.path}' ]; then
+            ${pkgs.gnupg}/bin/gpg --batch --import '${config.sops.secrets.gpg-private-key.path}'
+          fi
+        '';
+      };
+
+      custom.persist.user.directories = [
+        ".gnupg"
+      ];
+    };
 }
