d2359459723c535b668cab6f69aa1f3775b555c1
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-06-26 13:36:13 +0200

    refactor(cli): use env instead of runShell

--- modules/cli/gh.nix
+++ modules/cli/gh.nix
@@ -10,13 +10,10 @@
       gh = inputs.nix-wrapper-modules.lib.wrapPackage {
         inherit pkgs;
         package = pkgs.gh;
-        env.GH_CONFIG_DIR = pkgs.writeTextDir "/config.yml" (lib.strings.toJSON { version = 1; });
-        runShell = [
-          ''
-            GITHUB_TOKEN=$(cat "${config.sops.secrets.github-token.path}")
-            export GITHUB_TOKEN
-          ''
-        ];
+        env = {
+          GH_CONFIG_DIR = pkgs.writeTextDir "/config.yml" (lib.strings.toJSON { version = 1; });
+          GITHUB_TOKEN = "$(cat ${config.sops.secrets.github-token.path})";
+        };
       };
     in
     {
--- modules/cli/tack.nix
+++ modules/cli/tack.nix
@@ -9,12 +9,7 @@
       tack = inputs.nix-wrapper-modules.lib.wrapPackage {
         inherit pkgs;
         package = inputs.tack.packages.${pkgs.stdenv.hostPlatform.system}.tack;
-        runShell = [
-          ''
-            GITHUB_TOKEN=$(cat "${config.sops.secrets.github-token.path}")
-            export GITHUB_TOKEN
-          ''
-        ];
+        env.GITHUB_TOKEN = "$(cat ${config.sops.secrets.github-token.path})";
       };
     in
     {
