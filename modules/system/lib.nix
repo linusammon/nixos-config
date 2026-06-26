47ba24da513bfb77d52d584749f7c0ccbae0a5f5
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-06-26 14:09:26 +0200

    refactor(lib): simplify collect; use strings for exclude

--- modules/system/lib.nix
+++ modules/system/lib.nix
@@ -31,31 +31,12 @@
         exclude ? [ ],
       }:
       let
-        getModuleFile =
-          value:
-          if value ? imports && builtins.isList value.imports && value.imports != [ ] then
-            (builtins.head value.imports)._file or null
-          else
-            null;
-
-        excludedFiles = builtins.filter (v: v != null) (map getModuleFile exclude);
-
         collectLeaves =
           value:
-          if builtins.isFunction value then
+          if value ? imports then
             [ value ]
-          else if builtins.isAttrs value then
-            if value ? imports then
-              let
-                f = getModuleFile value;
-              in
-              if f != null && builtins.elem f excludedFiles then [ ] else [ value ]
-            else
-              lib.flatten (lib.attrValues (lib.mapAttrs (_: collectLeaves) value))
-          else if builtins.isList value then
-            lib.flatten (map collectLeaves value)
           else
-            [ ];
+            lib.concatMap (name: collectLeaves value.${name}) (builtins.attrNames (removeAttrs value exclude));
       in
       {
         imports = collectLeaves subtree;
