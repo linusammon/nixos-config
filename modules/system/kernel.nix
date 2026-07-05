86eeb7235d72e784afe96e94328db543f57514dd
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-07-05 19:31:51 +0200

    feat(system): drop cachyos kernel

--- modules/system/kernel.nix
+++
@@ -1,7 +0,0 @@
-{ inputs, ... }:
-{
-  modules.nixos.system.kernel = { pkgs, ... }: {
-    nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ];
-    boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
-  };
-}
--- modules/system/settings.nix
+++ modules/system/settings.nix
@@ -18,12 +18,10 @@
         ];
 
         extra-substituters = [
-          "https://attic.xuyh0120.win/lantian"
           "https://linusammon.cachix.org"
         ];
 
         extra-trusted-public-keys = [
-          "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
           "linusammon.cachix.org-1:g0X9oaHIZ5j48dsqfu8ZEdAp6HA2pH7Buv5Ye698qfc="
         ];
       };
