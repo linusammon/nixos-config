47f658dc163c488a9893a7c46e683f0323e01535
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-07-05 12:25:05 +0200

    feat(gui/zeditor): add package variants

--- modules/gui/zeditor/zeditor.nix
+++ modules/gui/zeditor/zeditor.nix
@@ -4,23 +4,39 @@
   lib,
   ...
 }:
-{
-  packages.zeditor =
+let
+  dir = ./langs;
+
+  allLangs = lib.listToAttrs (
+    map (n: {
+      name = lib.removeSuffix ".nix" (lib.removePrefix "_" n);
+      value = dir + "/${n}";
+    }) (builtins.filter (n: lib.hasSuffix ".nix" n) (builtins.attrNames (builtins.readDir dir)))
+  );
+
+  loadLangs =
     pkgs:
+    builtins.foldl' (
+      acc: name: lib.recursiveUpdate acc (import allLangs.${name} { inherit pkgs lib; })
+    ) { };
+
+  mkPkg =
+    langs:
+    { pkgs, ... }:
     inputs.nix-wrapper-modules.lib.wrapPackage {
       inherit pkgs;
       package = pkgs.zed-editor;
       env.XDG_CONFIG_HOME = "${placeholder "out"}/";
       constructFiles = {
-        settings = {
-          relPath = "zed/settings.json";
-          content = builtins.toJSON (
-            lib.recursiveUpdate (import ./_settings.nix config) (import ./_languages.nix pkgs lib)
-          );
-        };
         keymap = {
           relPath = "zed/keymap.json";
           content = builtins.toJSON (import ./_keymap.nix);
+        };
+        settings = {
+          relPath = "zed/settings.json";
+          content = builtins.toJSON (
+            lib.recursiveUpdate (import ./_settings.nix config) (loadLangs pkgs langs)
+          );
         };
         theme = {
           relPath = "zed/themes/base16.json";
@@ -28,15 +44,21 @@
         };
       };
     };
+in
+{
+  packages = lib.mapAttrs (_: mkPkg) (
+    {
+      zeditor = [ "nix" ];
+      zeditor-full = builtins.attrNames allLangs;
+      zeditor-minimal = [ ];
+    }
+    // lib.mapAttrs (name: _: [ name ]) allLangs
+  );
 
   modules.nixos.gui.zeditor =
-    {
-      pkgs,
-      lib,
-      ...
-    }:
+    { pkgs, lib, ... }:
     let
-      pkg = config.packages.zeditor pkgs;
+      pkg = config.packages.zeditor { inherit pkgs; };
     in
     {
       environment.systemPackages = [ pkg ];
