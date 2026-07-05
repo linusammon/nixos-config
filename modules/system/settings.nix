7e9763a5d14d8f47234012471437f14e54658a3e
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-07-05 19:02:53 +0200

    feat(system/settings): drop noctalia binary cache

--- modules/system/settings.nix
+++ modules/system/settings.nix
@@ -19,13 +19,11 @@
 
         extra-substituters = [
           "https://attic.xuyh0120.win/lantian"
-          "https://noctalia.cachix.org"
           "https://linusammon.cachix.org"
         ];
 
         extra-trusted-public-keys = [
           "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-          "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
           "linusammon.cachix.org-1:g0X9oaHIZ5j48dsqfu8ZEdAp6HA2pH7Buv5Ye698qfc="
         ];
       };
