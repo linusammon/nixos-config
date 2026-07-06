67c3fa892fe5493f9053a775c026532beaf29ead
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-07-06 19:41:14 +0200

    refactor: simplify recursive type and tighten option types

--- top.nix
+++ top.nix
@@ -1,50 +1,15 @@
 { lib, ... }:
 let
-  inherit (lib)
-    mkOptionType
-    mkOption
-    mergeDefinitions
-    ;
+  inherit (lib) mkOption types;
 
-  recursive = mkOptionType {
-    name = "recursive";
-    check = v: lib.isFunction v || lib.isAttrs v;
-    merge =
-      loc: defs:
-      if lib.all (d: lib.isFunction d.value) defs then
-        lib.types.deferredModule.merge loc defs
-      else
-        let
-          expanded = lib.concatMap (
-            d:
-            lib.mapAttrsToList (key: val: {
-              inherit (d) file;
-              name = key;
-              value = val;
-            }) d.value
-          ) (lib.filter (d: lib.isAttrs d.value) defs);
-
-          byKey = lib.groupBy (d: d.name) expanded;
-
-          mergeKey =
-            key: keyDefs:
-            let
-              keyDefsClean = map (d: {
-                inherit (d) file;
-                inherit (d) value;
-              }) keyDefs;
-            in
-            (mergeDefinitions (loc ++ [ key ]) recursive keyDefsClean).mergedValue;
-        in
-        lib.mapAttrs mergeKey byKey;
-  };
+  recursive = with types; (lazyAttrsOf (either recursive deferredModule)) // { description = ""; };
 in
 {
-  options = with lib.types; {
+  options = with types; {
     modules = mkOption { type = recursive; };
-    theme = mkOption { type = lazyAttrsOf unspecified; };
-    lib = mkOption { type = lazyAttrsOf unspecified; };
-    nixosConfigurations = mkOption { type = lazyAttrsOf unspecified; };
+    theme = mkOption { type = lazyAttrsOf anything; };
+    lib = mkOption { type = lazyAttrsOf (functionTo raw); };
+    nixosConfigurations = mkOption { type = lazyAttrsOf raw; };
     packages = mkOption { type = lazyAttrsOf (functionTo package); };
     formatter = mkOption { type = functionTo package; };
   };
