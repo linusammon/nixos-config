c02cdb200c08f944b0a7cd25119671e7b59703f4
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-07-07 15:10:54 +0200

    feat(gui/zeditor): combine everything into one package

--- modules/gui/zeditor/zeditor.nix
+++ modules/gui/zeditor/zeditor.nix
@@ -4,29 +4,9 @@
   lib,
   ...
 }:
-let
-  dir = ./langs;
-
-  allLangs =
-    dir
-    |> builtins.readDir
-    |> builtins.attrNames
-    |> builtins.filter (n: lib.hasSuffix ".nix" n)
-    |> map (n: {
-      name = n |> lib.removePrefix "_" |> lib.removeSuffix ".nix";
-      value = dir + "/${n}";
-    })
-    |> lib.listToAttrs;
-
-  loadLangs =
+{
+  packages.zeditor =
     pkgs:
-    builtins.foldl' (
-      acc: name: lib.recursiveUpdate acc (import allLangs.${name} { inherit pkgs lib; })
-    ) { };
-
-  mkPkg =
-    langs:
-    { pkgs, ... }:
     inputs.nix-wrapper-modules.lib.wrapPackage {
       inherit pkgs;
       package = pkgs.zed-editor;
@@ -39,7 +19,7 @@ let
         settings = {
           relPath = "zed/settings.json";
           content = builtins.toJSON (
-            lib.recursiveUpdate (import ./_settings.nix config) (loadLangs pkgs langs)
+            lib.recursiveUpdate (import ./_settings.nix config) (import ./_langs.nix pkgs lib)
           );
         };
         theme = {
@@ -48,20 +28,11 @@ let
         };
       };
     };
-in
-{
-  packages = lib.mapAttrs (_: mkPkg) (
-    {
-      zeditor = [ ];
-      zeditor-full = builtins.attrNames allLangs;
-    }
-    // lib.mapAttrs' (name: _: lib.nameValuePair "zeditor-${name}" [ name ]) allLangs
-  );
 
   modules.nixos.gui.zeditor =
     { pkgs, lib, ... }:
     let
-      pkg = config.packages.zeditor-nix { inherit pkgs; };
+      pkg = config.packages.zeditor pkgs;
     in
     {
       environment.systemPackages = [ pkg ];
