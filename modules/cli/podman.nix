e8cd702211a71f66e50f90e2b0cd52e091cce5d4
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-09-23 20:14:13 +0200

    feat(cli/podman): add podman compose

--- modules/cli/podman.nix
+++ modules/cli/podman.nix
@@ -1,10 +1,15 @@
 {
-  modules.nixos.cli.podman = { args, ... }: {
+  modules.nixos.cli.podman = { args, pkgs, ... }: {
     virtualisation.podman = {
       enable = true;
       dockerCompat = true;
     };
 
     users.users.${args.user}.extraGroups = [ "podman" ];
+
+    environment = {
+      systemPackages = with pkgs; [ podman-compose ];
+      sessionVariables.PODMAN_COMPOSE_WARNING_LOGS = "false";
+    };
   };
 }
